92a40fd2ae917f5ffa8720ce56d855ba2db2ee77
PeeOops <liem.richiee@gmail.com>
2025-05-30 10:37:04 +0700

    Fixed react-router instead using react-router-dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
-import Navigation from './components/Navigation'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+
 )
