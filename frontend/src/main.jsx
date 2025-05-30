384d00fecd4af2be1de00b4d0ee2342145b3a6c5
PeeOops <liem.richiee@gmail.com>
2025-05-30 11:14:46 +0700

    Added 404 Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NotFound from './components/404'
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 
