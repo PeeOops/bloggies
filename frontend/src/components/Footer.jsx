9b9b4bf427b41ac5702d275c21235002c10f2075
PeeOops <liem.richiee@gmail.com>
2025-05-30 10:27:14 +0700

    Fixed UI for Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { faX } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
     return(
         <footer className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-4 bg-emerald-950 text-white border-t-2 p-4 md:p-8">
-            <h1 className="text-3xl">Bloggies</h1>
+            <h1 role="button" className="text-3xl">Bloggies</h1>
             <div className="flex flex-col justify-center items-center gap-4">
                 <div className="flex flex-col items-center gap-2">
                     <p>Get our latest gaming news</p>
@@ -14,19 +14,19 @@ const Footer = () => {
                 </div>
                 {/* Links */}
                 <div className="flex flex-wrap text-gray-400 gap-2">
-                    <p>News</p>
-                    <p>Blogs</p>
-                    <p>Contact</p>
+                    <p role="button">News</p>
+                    <p role="button">Blogs</p>
+                    <p role="button">Contact</p>
                 </div>
                 {/* Social media icons */}
                 <div className="flex flex-wrap gap-2">
-                    <FontAwesomeIcon icon={faFacebook} />
-                    <FontAwesomeIcon icon={faInstagram} />
-                    <FontAwesomeIcon icon={faTwitter} />
-                    <FontAwesomeIcon icon={faTwitch} />
-                    <FontAwesomeIcon icon={faReddit} />
-                    <FontAwesomeIcon icon={faYoutube} />
-                    <FontAwesomeIcon icon={faTiktok} />
+                    <FontAwesomeIcon role="button" icon={faFacebook} />
+                    <FontAwesomeIcon role="button" icon={faInstagram} />
+                    <FontAwesomeIcon role="button" icon={faTwitter} />
+                    <FontAwesomeIcon role="button" icon={faTwitch} />
+                    <FontAwesomeIcon role="button" icon={faReddit} />
+                    <FontAwesomeIcon role="button" icon={faYoutube} />
+                    <FontAwesomeIcon role="button" icon={faTiktok} />
                 </div>
             </div>
             <p className="text-xs md:text-md">Copyright &copy; <span className="text-gray-400">PeeDegrees</span></p>
