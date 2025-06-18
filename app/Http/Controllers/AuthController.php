57b100038f2ce962d75632ea9ec31d11ca8b0aec
PeeOops <liem.richiee@gmail.com>
2025-06-18 14:19:31 +0700

    Added register API on backend

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            "username" => "required|unique:users",
+            "email" => "required|unique:users|email",
+            "password" => "required|min:6"
+
+        ]);
+
+        $user = User::create([
+            "username" => $request->username,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        return response()->json(["message" => "User registered successfully"]);
+    }
+}
