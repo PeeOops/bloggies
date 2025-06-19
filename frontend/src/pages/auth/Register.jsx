dc7801a48efcf9c3986cef1ec50a58ae20747860
PeeOops <liem.richiee@gmail.com>
2025-06-19 15:57:25 +0700

    Fixed to password matching on register page instead of show password

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import TopBackground from "../../assets/images/background.jpg"
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash, faWarning } from "@fortawesome/free-solid-svg-icons";
 
 const Register = () => {
 
@@ -12,36 +12,52 @@ const Register = () => {
         email: "",
         password: ""
     });
-    const confirmPasswordRef = useRef();
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     // Error message
     const [error, setError] = useState("");
 
-    // Show password
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    // Check password
+    const [passwordMessage, setPasswordMessage] = useState("");
 
     // Handle change
-    const handleChange = (e) => {
+    const handleChangeForm = (e) => {
         setForm({
             ...form,
             [e.target.name] : e.target.value
         })
     }
 
-    // Show password
-    const handleClickShowPassword = () => {
-        setShowPassword(prev => !prev);
+    const handleChangeConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value)
     }
+
+
+
+    useEffect(() => {
+        // Check match password
+        const handleChangeMatchPassword = () => {
+            if(confirmPassword && form.password !== confirmPassword){
+                setPasswordMessage("Passwords do not match!");
+            }else{
+                setPasswordMessage("");
+            }
+        }
+
+        handleChangeMatchPassword();
+    },[form.password, confirmPassword])
+
 
     // Form submit
     const handleSubmitForm = (e) => {
         e.preventDefault();
 
-        if(confirmPasswordRef.current.value !== form.password){
-            alert("Password doesn't match");
+        if (form.password !== confirmPassword) {
+            setPasswordMessage("Passwords do not match!");
             return;
         }
+
+        setPasswordMessage("");
 
         try {
             
@@ -58,24 +74,36 @@ const Register = () => {
                     <p className="md:text-2xl">Discover the latest in gaming news and articles.</p>
                 </div>
                 <div className="flex flex-col md:w-1/2 md:justify-center md:items-center py-8 px-12 gap-2 md:gap-4">
+                    {/* Error message */}
+                    {
+                        passwordMessage === "Passwords do not match!" ?
+                        <div className="flex flex-row items-center gap-2 text-sm text-red-400">
+                            <FontAwesomeIcon icon={faWarning} />
+                            <p>Passwords do not match!</p>
+                        </div> : ""
+                    }
+                    
                     <h1 className="text-xl md:text-3xl">Sign Up</h1>
                     <form onSubmit={handleSubmitForm} className="flex flex-col gap-4 md:gap-6 md:text-lg">
                         <div className="flex flex-col">
                             <label>Email</label>
-                            <input name="email" type="email" placeholder="email" className="bg-transparent text-emerald-950 p-2 border-2 border-emerald-950 rounded-md focus:outline-none focus:border-b-2 focus:border-b-emerald-950" onPaste={(e) => e.preventDefault()} onChange={handleChange} value={form.email} required />
+                            <input name="email" type="email" placeholder="email" className="bg-transparent text-emerald-950 p-2 border-2 border-emerald-950 rounded-md focus:outline-none focus:border-b-2 focus:border-b-emerald-950" onPaste={(e) => e.preventDefault()} onChange={handleChangeForm} value={form.email} required />
                         </div>
                         <div className="flex flex-col">
                             <label>Username</label>
-                            <input name="username" type="text" placeholder="username" className="bg-transparent text-emerald-950 p-2 border-2 border-emerald-950 rounded-md focus:outline-none focus:border-b-2 focus:border-b-emerald-950" onPaste={(e) => e.preventDefault()} onChange={handleChange} value={form.username} required />
+                            <input name="username" type="text" placeholder="username" className="bg-transparent text-emerald-950 p-2 border-2 border-emerald-950 rounded-md focus:outline-none focus:border-b-2 focus:border-b-emerald-950" onPaste={(e) => e.preventDefault()} onChange={handleChangeForm} value={form.username} required />
                         </div>
                         <div className="flex flex-col">
                             <label>Password</label>
-                            <input name="password" type={!showPassword ? "password" : "text"} placeholder="password" className="bg-transparent text-emerald-950 p-2 border-2 border-emerald-950 rounded-md focus:outline-none focus:border-b-2 focus:border-b-emerald-950" onPaste={(e) => e.preventDefault()} onChange={handleChange} value={form.password} required />
-                            <FontAwesomeIcon onClick={() => handleClickShowPassword()} className="absolute right-15 bottom-3 md:right-0 md:bottom-0 cursor-pointer" icon={!showPassword ? faEye : faEyeSlash} />
+                            <input name="password" type="password" placeholder="password" className={`bg-transparent text-emerald-950 p-2 border-2 rounded-md focus:outline-none focus:border-b-2 ${
+                            passwordMessage ? 'border-red-400 focus:border-b-red-400' : 'border-emerald-950 focus:border-b-emerald-950'
+                            }`} onPaste={(e) => e.preventDefault()} onChange={handleChangeForm} value={form.password} required />
                         </div>
                         <div className="flex flex-col">
                             <label>Confirm Password</label>
-                            <input type="password" placeholder="confirm password" className="bg-transparent text-emerald-950 p-2 border-2 border-emerald-950 rounded-md focus:outline-none focus:border-b-2 focus:border-b-emerald-950" onPaste={(e) => e.preventDefault()} ref={confirmPasswordRef}  required />
+                            <input type="password" placeholder="confirm password" className={`bg-transparent text-emerald-950 p-2 border-2 rounded-md focus:outline-none focus:border-b-2 ${
+                            passwordMessage ? 'border-red-400 focus:border-b-red-400' : 'border-emerald-950 focus:border-b-emerald-950'
+                            }`} onPaste={(e) => e.preventDefault()} onChange={handleChangeConfirmPassword}  required />
                         </div>
                         <button className="bg-emerald-950 text-white p-2 rounded-md active:bg-white active:text-emerald-950 active:border-emerald-950 active:border-2 hover:bg-white hover:text-emerald-950 hover:border-emerald-950 hover:border-2 cursor-pointer">Sign In</button>
                         <p className="text-xs text-center">Already have an account? <Link to="/login" className="text-gray-400 cursor-pointer" role="button">Sign In</Link></p>
