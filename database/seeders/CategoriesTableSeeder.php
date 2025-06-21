1ed1d15ba165e1eb5803c9051d0ce111d66a8865
PeeOops <liem.richiee@gmail.com>
2025-06-21 09:54:17 +0700

    Added Category table and categories seeder

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ["name" => "Game releases & updates"],
+            ["name" => "Reviews & critiques"],
+            ["name" => "eSports & competition"],
+            ["name" => "Guides & tips"],
+            ["name" => "Hardware & accessories"]
+        ];
+        
+        foreach ($categories as $category){
+            Category::create($category);
+        }
+    }
+}
