f018707c691f1b8c099be8c2967032a91349a11c
PeeOops <liem.richiee@gmail.com>
2025-07-02 15:04:59 +0700

    Added logic for update profile

--- frontend/src/components/User/Profile.jsx
+++ frontend/src/components/User/Profile.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import api from "../../axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle, faWarning } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({userData, setUserData}) => {
 
+    const navigate = useNavigate();
     const [editing, setEditing] = useState(false);
+    const [message, setMessage] = useState("");
     const [form,setForm] = useState({
-        username : "",
-        email : "",
-        bio : "",
+        username : userData.username,
+        email : userData.email,
+        bio : userData.bio,
     })
 
     const handleClickEdit = () => {
@@ -20,26 +26,65 @@ const Profile = () => {
         })
     }
 
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        if(form.username === "" || form.email === "" || form.bio === ""){
+            setMessage("You need to fill all input fields");
+        }
+
+        setMessage("");
+
+        api.put("/update", form)
+        .then((res) => {
+            setMessage("Profile updated successfully!");
+            setUserData(res.data.user);
+            setEditing(false);
+            navigate(`/user/${form.username}`);
+        })
+        .catch((error) => {
+            const errorMessages = Object.values(error.response.data.errors).flat();        
+
+            if(error.response.status === 422){
+                setMessage(errorMessages);
+            }
+
+        })
+
+    }
+
     return (
-        <div className="flex flex-col gap-4">
+        <form onSubmit={handleSubmitForm} className="flex flex-col gap-4">
+            {
+                message === "Profile updated successfully!" ?
+                <div className="flex flex-row items-center gap-2 text-sm text-green-600">
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                    <p>Profile updated successfully!</p>
+                </div> : message !== "" ? 
+                <div className="flex flex-row items-center gap-2 text-sm text-red-600">
+                    <FontAwesomeIcon icon={faWarning} />
+                    <p>{message}</p>
+                </div> : ""
+            }
+            
             <div>
                 <p>Username</p>
-                <input type="text" placeholder="Mintymantis" className="text-gray-600 border-2 border-black p-1" onChange={handleChangeForm} value={form.username} disabled={editing ? false : true} />
+                <input name="username" type="text" className="text-gray-600 border-2 border-black p-1" onChange={handleChangeForm} value={form.username} disabled={!editing} required />
             </div>
             <div>
                 <p>Email</p>
-                <input type="email" placeholder="mintymantis@gmail.com" className="text-gray-600 border-2 border-black p-1" onChange={handleChangeForm} value={form.email} disabled={editing ? false : true} />
+                <input name="email" type="email" className="text-gray-600 border-2 border-black p-1" onChange={handleChangeForm} value={form.email} disabled={!editing} required />
             </div>
             <div>
                 <p>Bio</p>
-                <input type="text" placeholder="Reading" className="text-gray-600 border-2 border-black p-1" onChange={handleChangeForm} value={form.bio} disabled={editing ? false : true} />
+                <input name="bio" type="text" className="text-gray-600 border-2 border-black p-1" onChange={handleChangeForm} value={form.bio} disabled={!editing} required />
             </div>
             <div className="flex flex-row gap-2">
-                <button onClick={() => handleClickEdit()} className={`${editing ? "hidden" : ""} bg-emerald-950 text-white text-sm rounded-sm cursor-pointer py-1 px-2`}>Edit</button>
-                <button className={`${editing ? "" : "hidden"} bg-emerald-950 text-white text-sm rounded-sm cursor-pointer py-1 px-2`}>Save</button>
-                <button onClick={() => handleClickEdit()} className={`${editing ? "" : "hidden"} bg-red-600 text-white text-sm rounded-sm cursor-pointer py-1 px-2`}>Cancel</button>
+                <button type="button" onClick={() => handleClickEdit()} className={`${editing ? "hidden" : ""} bg-emerald-950 text-white text-sm rounded-sm cursor-pointer py-1 px-2`}>Edit</button>
+                <button type="submit" className={`${editing ? "" : "hidden"} bg-emerald-950 text-white text-sm rounded-sm cursor-pointer py-1 px-2`}>Save</button>
+                <button type="button" onClick={() => handleClickEdit()} className={`${editing ? "" : "hidden"} bg-red-600 text-white text-sm rounded-sm cursor-pointer py-1 px-2`}>Cancel</button>
             </div>
-        </div>
+        </form>
     )
 }
 
