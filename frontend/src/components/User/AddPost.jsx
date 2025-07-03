21d7bda582f003a3845de013a13848dacca1b4c1
PeeOops <liem.richiee@gmail.com>
2025-07-03 16:07:29 +0700

    Added handleChangeForm on Add Post

--- frontend/src/components/User/AddPost.jsx
+++ frontend/src/components/User/AddPost.jsx
@@ -1,12 +1,22 @@
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import api from "../../axios";
 
 const AddPost = ({userData}) => {
 
     const [categories, setCategories] = useState([]);
     const [tags, setTags] = useState([]);
+    const [form, setForm] = useState({
+        "title" : "",
+        "subtitle" : "",
+        "featured_image" : null,
+        "body" : "",
+        "category_id" : null,
+        "tag_ids" : [],
+        "author_id" : userData.id
+        
+    })
 
     useEffect(() => {
         // Fetch categories
@@ -33,25 +43,70 @@ const AddPost = ({userData}) => {
         fetchTags();
     },[])
 
+
+    // Handle form change
+    const handleChangeForm = (e) => {
+        if(e.target.type === "file"){
+            setForm({
+                ...form,
+                [e.target.name] : e.target.files[0]
+            })
+        }else if (e.target.type === "checkbox" && e.target.name === "tag_ids") {
+            const tagId = parseInt(e.target.value); // convert string to number
+
+            setForm((prevForm) => ({
+            ...prevForm,
+            tag_ids: e.target.checked
+                ? [...prevForm.tag_ids, tagId]
+                : prevForm.tag_ids.filter((id) => id !== tagId)
+            }));
+        }else{
+            setForm({
+                ...form,
+                [e.target.name] : e.target.value
+            })
+        }
+        
+    }
+
+    // Submit form
+    const handleClickSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    // SimpleMDE markdown configuration
+    const handleMarkdownChange = useCallback((value) => {
+        setForm(prev => ({ ...prev, body: value }));
+    }, []);
+
+    const simpleMdeOptions = useMemo(() => ({
+        spellChecker: false,
+        lineWrapping: true,
+    }), []);
+
     return(
-        <form className="flex flex-col md:grid md:grid-cols-[3fr_1fr] gap-4">
-            
+        <form onSubmit={handleClickSubmit} className="flex flex-col md:grid md:grid-cols-[3fr_1fr] gap-4">
             <div className="flex flex-col gap-4">
                 {/* Title */}
                 <div className="flex flex-col gap-2">
                     <label>Title</label>
-                    <input type="text" placeholder="Title" className="text-gray-600 border-2 border-black p-1" />
+                    <input name="title" type="text" placeholder="Title" value={form.title} onChange={handleChangeForm} className="text-gray-600 border-2 border-black p-1" />
                 </div>
                 {/* Subtitle */}
                 <div className="flex flex-col gap-2">
                     <label>Subtitle</label>
-                    <input type="text" placeholder="Subtitle" className="text-gray-600 border-2 border-black p-1" />
+                    <input name="subtitle" type="text" value={form.subtitle} onChange={handleChangeForm} placeholder="Subtitle" className="text-gray-600 border-2 border-black p-1" />
                 </div>
                 {/* Featured image */}
                 <div className="flex flex-col gap-2">
                     <label>Featured image</label>
-                    <span className="ml-4 text-sm">No Upload</span>
-                    <input type="file" id="file" className="hidden" />
+                    <div className="flex flex-row gap-2 items-center">
+                        {
+                            form.featured_image ? <span className="text-sm">File: <span className="text-sm">{form.featured_image.name}</span></span> : <span className="text-sm">No Upload</span>
+                        }
+                        
+                    </div>    
+                    <input name="featured_image" type="file" onChange={handleChangeForm} id="file" className="hidden" />
                     <label
                         htmlFor="file"
                         className="text-sm inline-block bg-emerald-950 text-white px-4 py-2 rounded cursor-pointer hover:bg-white hover:text-emerald-950 hover:border-1 hover:border-emerald-950"
@@ -64,12 +119,9 @@ const AddPost = ({userData}) => {
                     <label>Post</label>
                     <div className="max-w-xl overflow-auto">
                         <SimpleMDE
-                            value=""
-                            onChange=""
-                            options={{
-                            spellChecker: false,
-                            lineWrapping: true,
-                            }}
+                            value={form.body}
+                            onChange={handleMarkdownChange}
+                            options={simpleMdeOptions}
                         />
                     </div>
                 </div>
@@ -77,8 +129,8 @@ const AddPost = ({userData}) => {
             <div className="flex flex-col gap-4">
                 {/* Category */}
                 <div className="flex flex-col gap-2">
-                    <label for="category">Category:</label>
-                    <select id="category" name="category" className="border-2 p-1">
+                    <label htmlFor="category">Category:</label>
+                    <select id="category" name="category_id" onChange={handleChangeForm} className="border-2 p-1">
                         <option value="" hidden>Choose Category</option>
                         {
                             categories.map((category) => (
@@ -89,11 +141,13 @@ const AddPost = ({userData}) => {
                 </div>
                 {/* Tags */}
                 <div className="flex flex-col gap-2">
-                    <label for="tags">Tags:</label>
+                    <label>Tags:</label>
                     {
                         tags.map((tag) => (
                             <div key={tag.id} className="flex flex-row gap-2">
-                                <input id={`tag-${tag.id}`} name="tag" type="checkbox" value={tag.id} />
+                                <input id={`tag-${tag.id}`} name="tag_ids" type="checkbox" value={tag.id}
+                                checked={form.tag_ids.includes(tag.id)}
+                                onChange={handleChangeForm} />
                                 <label htmlFor={`tag-${tag.id}`}>{tag.name}</label>
                             </div>
                         ))
