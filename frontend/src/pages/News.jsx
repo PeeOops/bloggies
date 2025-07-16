c8aa52951e4b9cb66612a5b25d177d28f0492640
PeeOops <liem.richiee@gmail.com>
2025-07-16 22:57:32 +0700

    Added recent news section for news page

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -5,8 +5,30 @@ import Navigation from "../components/Navigation";
 import { faAnglesRight, faArrowRight, faCalendar, faUser } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram, faReddit, faTiktok, faTwitch, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import api from "../axios";
+import { Link } from "react-router-dom";
 
 const News = () => {
+
+    const [recentNewsPost, setRecentNewsPost] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [recentNewsAPI] = await Promise.all([
+                    api.get("/post/index")
+                ])
+                setRecentNewsPost(recentNewsAPI.data.posts.filter((post) => post.type === "News"));
+
+            } catch (error) {
+                
+            }
+        }
+
+        fetchData();
+    },[])
+
     return(
         <>
             {/* Header */}
@@ -27,140 +49,71 @@ const News = () => {
                 <h1 className="text-xl md:text-3xl text-white">Recent News</h1>
                 <div className="flex flex-col md:grid md:grid-cols-[2fr_3fr] gap-4">
                     {/* Newest */}
-                    <div role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
-                        <img src={Thumbnail} alt="" className="w-full h-48 md:h-96 object-cover" />
-                        <div className="p-4 md:p-6">
-                            {/* Published date and author */}
-                            <div className="flex flex-row text-xs md:text-md gap-2">
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                    <p>28th May 2025</p>
+                    {
+                        recentNewsPost.slice(0,1).map((post) => (
+                            <Link to={`/post/${post.id}`} key={post.id} role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
+                                <img src={`http://localhost:8000/storage/${post.featured_image_url}`} alt={post.title} className="w-full h-48 md:h-96 object-cover" />
+                                <div className="p-4 md:p-6">
+                                    {/* Published date and author */}
+                                    <div className="flex flex-row text-xs md:text-md gap-2">
+                                        <div className="flex flex-row gap-2 items-center">
+                                            <FontAwesomeIcon icon={faCalendar} />
+                                            <p>{post.created_at}</p>
+                                        </div>
+                                        <span>|</span>
+                                        <div className="flex flex-row gap-2 items-center">
+                                            <FontAwesomeIcon icon={faUser} />
+                                            <p>{post.author.username}</p>
+                                        </div>
+                                    </div>
+                                    {/* Title */}
+                                    <h1 className="text-lg md:text-2xl line-clamp-2 mt-2">{post.title}</h1>
+                                    {/* Subtitle or Description */}
+                                    <p className="line-clamp-5 text-xs md:text-base">{post.subtitle}</p>
+                                    {/* Read more button */}
+                                    <div className="flex flex-row gap-2 items-center text-xs mt-2">
+                                        <FontAwesomeIcon icon={faAnglesRight} />
+                                        <p className="text-sm md:text-xl">Read more</p>
+                                    </div>
                                 </div>
-                                <span>|</span>
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faUser} />
-                                    <p>ELLIOT ALDERSON</p>
-                                </div>
-                            </div>
-                            {/* Title */}
-                            <h1 className="text-lg md:text-2xl line-clamp-2 mt-2">One of the best steam games in 2025</h1>
-                            {/* Subtitle or Description */}
-                            <p className="line-clamp-5 text-xs md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, excepturi ratione animi error ducimus atque laborum fugiat iusto sequi blanditiis, illo temporibus aut consequuntur nulla earum, laudantium perferendis inventore placeat.</p>
-                            {/* Read more button */}
-                            <div className="flex flex-row gap-2 items-center text-xs mt-2">
-                                <FontAwesomeIcon icon={faAnglesRight} />
-                                <p className="text-sm md:text-xl">Read more</p>
-                            </div>
-                        </div>
-                    </div>
+                            </Link>
+                        ))
+                    }
+                    
                     {/* 2nd to 4th Recent News */}
                     <div className="grid grid-cols-2 gap-2 md:gap-4">
-                        <div role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
-                        <img src={Thumbnail} alt="" className="w-full h-24 md:h-36 object-cover" />
-                        <div className="p-4 md:p-6">
-                            {/* Published date and author */}
-                            <div className="hidden md:flex flex-row text-xs gap-2">
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                    <p>28th May 2025</p>
-                                </div>
-                                <span>|</span>
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faUser} />
-                                    <p>ELLIOT ALDERSON</p>
-                                </div>
-                            </div>
-                            {/* Title */}
-                            <h1 className="text-sm md:text-lg line-clamp-2 md:mt-2">One of the best steam games in 2025</h1>
-                            {/* Subtitle or Description */}
-                            <p className="hidden md:line-clamp-3 text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, excepturi ratione animi error ducimus atque laborum fugiat iusto sequi blanditiis, illo temporibus aut consequuntur nulla earum, laudantium perferendis inventore placeat.</p>
-                            {/* Read more button */}
-                            <div className="flex flex-row gap-2 items-center text-xs mt-2">
-                                <FontAwesomeIcon icon={faAnglesRight} />
-                                <p>Read more</p>
-                            </div>
-                        </div>
-                        </div>
-                        <div role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
-                        <img src={Thumbnail} alt="" className="w-full h-24 md:h-36 object-cover" />
-                        <div className="p-4 md:p-6">
-                            {/* Published date and author */}
-                            <div className="hidden md:flex flex-row text-xs gap-2">
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                    <p>28th May 2025</p>
-                                </div>
-                                <span>|</span>
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faUser} />
-                                    <p>ELLIOT ALDERSON</p>
-                                </div>
-                            </div>
-                            {/* Title */}
-                            <h1 className="text-sm md:text-lg line-clamp-2 md:mt-2">One of the best steam games in 2025</h1>
-                            {/* Subtitle or Description */}
-                            <p className="hidden md:line-clamp-3 text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, excepturi ratione animi error ducimus atque laborum fugiat iusto sequi blanditiis, illo temporibus aut consequuntur nulla earum, laudantium perferendis inventore placeat.</p>
-                            {/* Read more button */}
-                            <div className="flex flex-row gap-2 items-center text-xs mt-2">
-                                <FontAwesomeIcon icon={faAnglesRight} />
-                                <p>Read more</p>
-                            </div>
-                        </div>
-                        </div>
-                        <div role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
-                        <img src={Thumbnail} alt="" className="w-full h-24 md:h-36 object-cover" />
-                        <div className="p-4 md:p-6">
-                            {/* Published date and author */}
-                            <div className="hidden md:flex flex-row text-xs gap-2">
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                    <p>28th May 2025</p>
-                                </div>
-                                <span>|</span>
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faUser} />
-                                    <p>ELLIOT ALDERSON</p>
-                                </div>
-                            </div>
-                            {/* Title */}
-                            <h1 className="text-sm md:text-lg line-clamp-2 md:mt-2">One of the best steam games in 2025</h1>
-                            {/* Subtitle or Description */}
-                            <p className="hidden md:line-clamp-3 text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, excepturi ratione animi error ducimus atque laborum fugiat iusto sequi blanditiis, illo temporibus aut consequuntur nulla earum, laudantium perferendis inventore placeat.</p>
-                            {/* Read more button */}
-                            <div className="flex flex-row gap-2 items-center text-xs mt-2">
-                                <FontAwesomeIcon icon={faAnglesRight} />
-                                <p>Read more</p>
-                            </div>
-                        </div>
-                        </div>
-                        <div role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
-                        <img src={Thumbnail} alt="" className="w-full h-24 md:h-36 object-cover" />
-                        <div className="p-4 md:p-6">
-                            {/* Published date and author */}
-                            <div className="hidden md:flex flex-row text-xs gap-2">
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faCalendar} />
-                                    <p>28th May 2025</p>
-                                </div>
-                                <span>|</span>
-                                <div className="flex flex-row gap-2 items-center">
-                                    <FontAwesomeIcon icon={faUser} />
-                                    <p>ELLIOT ALDERSON</p>
-                                </div>
-                            </div>
-                            {/* Title */}
-                            <h1 className="text-sm md:text-lg line-clamp-2 md:mt-2">One of the best steam games in 2025</h1>
-                            {/* Subtitle or Description */}
-                            <p className="hidden md:line-clamp-3 text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, excepturi ratione animi error ducimus atque laborum fugiat iusto sequi blanditiis, illo temporibus aut consequuntur nulla earum, laudantium perferendis inventore placeat.</p>
-                            {/* Read more button */}
-                            <div className="flex flex-row gap-2 items-center text-xs mt-2">
-                                <FontAwesomeIcon icon={faAnglesRight} />
-                                <p>Read more</p>
-                            </div>
-                        </div>
+                        {
+                            recentNewsPost.slice(1,5).map((post) => (
+                                <Link to={`/post/${post.id}`} key={post.id} role="button" className="flex flex-col bg-emerald-950 border-1 border-white shadow-white shadow-md cursor-pointer transition transform duration-150 active:scale-90 hover:scale-105 focus:outline-none text-white">
+                                    <img src={`http://localhost:8000/storage/${post.featured_image_url}`} alt={post.title} className="w-full h-24 md:h-36 object-cover" />
+                                    <div className="p-4 md:p-6">
+                                        {/* Published date and author */}
+                                        <div className="hidden md:flex flex-row text-xs gap-2">
+                                            <div className="flex flex-row gap-2 items-center">
+                                                <FontAwesomeIcon icon={faCalendar} />
+                                                <p>{post.created_at}</p>
+                                            </div>
+                                            <span>|</span>
+                                            <div className="flex flex-row gap-2 items-center">
+                                                <FontAwesomeIcon icon={faUser} />
+                                                <p>{post.author.username}</p>
+                                            </div>
+                                        </div>
+                                        {/* Title */}
+                                        <h1 className="text-sm md:text-lg line-clamp-2 md:mt-2">{post.title}</h1>
+                                        {/* Subtitle or Description */}
+                                        <p className="hidden md:line-clamp-2 text-xs">{post.subtitle}</p>
+                                        {/* Read more button */}
+                                        <div className="flex flex-row gap-2 items-center text-xs mt-2">
+                                            <FontAwesomeIcon icon={faAnglesRight} />
+                                            <p>Read more</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))
+                        }
                         </div>
                     </div>
-                </div>
                 <p role="button" className="text-white text-end cursor-pointer">Show more...</p>
             </div>
             {/* Popular News */}
