9a91117f5935d320a19e7c64c0dcd168b4f9ceed
PeeOops <liem.richiee@gmail.com>
2025-07-18 11:04:32 +0700

    Added URLSearch for navigation on user page

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -4,16 +4,17 @@ import Footer from "../components/Footer";
 import Profile from "../components/User/Profile";
 import { useEffect, useState } from "react";
 import AddPost from "../components/User/AddPost";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import api from "../axios";
 import Unauthorized from "../components/Errors/403";
 
 const User = () => {
 
-    const [navigation, setNavigation] = useState("Profile");
+    const [navigation, setNavigation] = useState("profile");
     const {username} = useParams();
     const [userData, setUserData] = useState({});
     const [authorized, setAuthorized] = useState(null);
+    const [searchParams, setSearchParams] = useSearchParams();
 
    useEffect(() => {
     const token = localStorage.getItem("token");
@@ -46,8 +47,14 @@ const User = () => {
 
     const handleClickNavigate = (nav) => {
         setNavigation(nav);
+        if(nav){
+            const newSearchParams = new URLSearchParams(searchParams);
+            newSearchParams.set("nav", nav);
+            setSearchParams(newSearchParams)
+        }
     }
 
+    // Loading screen
     if(authorized === null){
         return(
             <>
@@ -75,6 +82,7 @@ const User = () => {
         )
     }
 
+    // Unauthorized user
     if(!authorized){
         return(
             <>
@@ -83,7 +91,7 @@ const User = () => {
         )
     }
     
-
+    // Authorized user
     if(authorized){
         return (
             <>
@@ -112,19 +120,19 @@ const User = () => {
                         </div>
                         
                         <div className="flex flex-col gap-4">
-                            <p onClick={() => handleClickNavigate("Profile")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "Profile" ? "border-l-3 bg-gray-200" : ""}`} role="button">Profile</p>
-                            <p onClick={() => handleClickNavigate("Add Post")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "Add Post" ? "border-l-3 bg-gray-200" : ""}`} role="button">Add Post</p>
-                            <p onClick={() => handleClickNavigate("Saved Articles")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "Saved Articles" ? "border-l-3 bg-gray-200" : ""}`} role="button">Saved Articles</p>
-                            <p onClick={() => handleClickNavigate("Activities")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "Activities" ? "border-l-3 bg-gray-200" : ""}`} role="button">Activities</p>
+                            <p onClick={() => handleClickNavigate("profile")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "profile" ? "border-l-3 bg-gray-200" : ""}`} role="button">Profile</p>
+                            <p onClick={() => handleClickNavigate("add post")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "add post" ? "border-l-3 bg-gray-200" : ""}`} role="button">Add Post</p>
+                            <p onClick={() => handleClickNavigate("saved articles")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "saved articles" ? "border-l-3 bg-gray-200" : ""}`} role="button">Saved Articles</p>
+                            <p onClick={() => handleClickNavigate("activities")} className={`p-2 hover:border-l-3 hover:bg-gray-200 cursor-pointer ${navigation === "activities" ? "border-l-3 bg-gray-200" : ""}`} role="button">Activities</p>
                         </div>
                     </div>
 
                     {/* Details */}
                     <div className="bg-white text-emerald-950 px-4 py-2 shadow-gray-400 shadow-md">
-                        <h1 className="text-xl mb-6">{navigation}</h1>
+                        <h1 className="text-xl mb-6">{navigation.charAt(0).toUpperCase() + navigation.slice(1)}</h1>
                         {/* Profile Content */}
                         {
-                            navigation === "Profile" ? <Profile userData={userData} setUserData={setUserData} /> : navigation === "Add Post" ? <AddPost userData={userData} /> : ""
+                            navigation === "profile" ? <Profile userData={userData} setUserData={setUserData} /> : navigation === "add post" ? <AddPost userData={userData} /> : ""
                         }
                     </div>
                 </div>
