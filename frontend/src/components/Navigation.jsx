f94c9638ee239290d4d1ea4adfb1f33ee37e9a28
PeeOops <liem.richiee@gmail.com>
2025-07-24 09:26:08 +0700

    Added fallback loading when fetching username on navbar and added loading bar on logout

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -3,15 +3,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import api from "../axios.js";
+import LoadingBar from "./Utils/LoadingBar.jsx";
 
 const Navigation = () => {
 
+    // State declarations
     const [isMenu, setIsMenu] = useState(false);
-    const token = localStorage.getItem("token");
     const navigate = useNavigate();
-
     const [user, setUser] = useState({});
 
+    // Check if auth token exist 
+    const token = localStorage.getItem("token");
+
+    // Loading bar
+    const [loading, setLoading] = useState(false);
+    const [progress, setProgress] = useState(0);
+
+    const simulateProgress = () => {
+        setProgress(10);
+
+        const interval = setInterval(() => {
+            setProgress((prev) => {
+                if(prev < 90){
+                    return prev + Math.random() * 10;
+                }
+                clearInterval(interval);
+                return prev;
+            })
+        },100)
+
+        return interval;
+
+    }
+
+    // Fetch user data for username
     useEffect(() => {
         const fetchUser = () => {
             if(token){
@@ -28,26 +53,45 @@ const Navigation = () => {
         fetchUser();
     },[])
 
+    // Handle click nav
     const handleClickNav = () => {
         setIsMenu(prev => !prev);
     }
 
+    // Handle click logout
     const handleClickLogout = () => {
+
+        setLoading(true);
+        setProgress(0);
+
+        const progressInterval = simulateProgress();
+
+
         api.post("/logout")
         .then((res) => {
             localStorage.removeItem("token");
             delete api.defaults.headers.common["Authorization"];
             setUser({});
             navigate("/login");
-            
         })
         .catch((error) => {
-            
-        });
+            console.log("Failed to logout", error);
+        })
+        .finally(() => {
+            clearInterval(progressInterval);
+            setProgress(100);
+            setTimeout(() => {
+                setLoading(false);
+                setProgress(0);
+            },500);
+        })
     }
 
     return(
         <>
+            {/* Loading bar */}
+            <LoadingBar loading={loading} progress={progress} />
+
             <div className="flex flex-col md:flex-row md:justify-between md:items-center text-white mx-4 md:mx-24 md:text-base">
                 <div className="flex flex-row items-center justify-between">
                     <FontAwesomeIcon onClick={() => handleClickNav()} role="button" icon={faBars} className="text-xl md:invisible cursor-pointer" />
@@ -71,7 +115,7 @@ const Navigation = () => {
                         </ul> :
                         <ul className="flex flex-col md:flex-row items-start md:items-center gap-4 bg-green-900 md:bg-transparent text-white p-2 border-white border-1 md:border-0 rounded-md shadow-white shadow-md md:rounded-none md:shadow-none">
                             <Link to={`/user/${user.username}`} role="button" className="cursor-pointer md:pb-0">
-                                {user.username}
+                                {user.username ? user.username : "Loading..."}
                             </Link>
                             <span className="border-1 w-full md:border-0"></span>
                             <li onClick={() => handleClickLogout()} role="button" className="cursor-pointer pb-2 md:pb-0"> Logout</li>
